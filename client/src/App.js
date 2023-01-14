f3e18fbc0272dd7f50dc6d4788bb626349988394
Goran SD <g.cvetic23@gmail.com>
2023-01-14 13:21:00 -0800

    redux store created with theme mode reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material/styles";
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+
 function App() {
-  return <div className="app">HELLO</div>;
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  return (
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
+  );
 }
 
 export default App;
