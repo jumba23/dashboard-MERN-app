00fb4ca955c43acdbe436974449d6069cb26bd66
Goran SD <g.cvetic23@gmail.com>
2023-01-14 13:59:54 -0800

    nav bar and initial layout started

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,37 @@
 import React, { useMemo } from "react";
-import { createTheme, CssBaseline, ThemeProvider } from "@mui/material/styles";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
 import { useSelector } from "react-redux";
-import { themeSettings } from "theme";
+import { themeSettings } from "./theme";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./scenes/layout/index.jsx";
+import Dashboard from "./scenes/dashboard/index.jsx";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
+
+  //The useMemo hook is a React hook that is used to cache the value of a function that is expensive to compute.
+  //It takes two arguments: the function to compute, and an array of dependencies.
+  //The function is only re-computed if one of the dependencies changes.
+  //The theme object is expensive to create because it involves processing of the themeSettings,
+  // So, it's being wrapped with useMemo hook to make sure it's only created when the mode changes.
+  //By using useMemo hook, we can make sure that the theme object is only re-created when the theme mode changes,
+  // which is a more efficient way of managing the theme object.
+  // [mode] is a dependency
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
