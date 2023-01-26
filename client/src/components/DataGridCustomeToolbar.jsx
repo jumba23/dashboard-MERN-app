9142af446df2c0e7bcf90631f42c4274db575fcb
Goran SD <g.cvetic23@gmail.com>
2023-01-26 14:28:08 -0800

    tool bar added

--- client/src/components/DataGridCustomeToolbar.jsx
+++ client/src/components/DataGridCustomeToolbar.jsx
@@ -1,23 +1,47 @@
 import React from "react";
+import { Search } from "@mui/icons-material";
+import { IconButton, TextField, InputAdornment } from "@mui/material";
 import {
-  GridColumnMenuContainer,
-  GridFilterMenuItem,
-  HideGridColMenuItem,
+  GridToolbarDensitySelector,
+  GridToolbarContainer,
+  GridToolbarExport,
+  GridToolbarColumnsButton,
 } from "@mui/x-data-grid";
+import FlexBetween from "./FlexBetween";
 
-const CustomColumnMenu = (props) => {
-  const { hideMenu, currentColumn, open } = props;
-  if (!currentColumn) return null;
+const DataGridCustomToolbar = ({ searchInput, setSearchInput, setSearch }) => {
   return (
-    <GridColumnMenuContainer
-      hideMenu={hideMenu}
-      currentColumn={currentColumn}
-      open={open}
-    >
-      <GridFilterMenuItem onClick={hideMenu} column={currentColumn} />
-      <HideGridColMenuItem onClick={hideMenu} column={currentColumn} />
-    </GridColumnMenuContainer>
+    <GridToolbarContainer>
+      <FlexBetween width="100%">
+        <FlexBetween>
+          <GridToolbarColumnsButton />
+          <GridToolbarDensitySelector />
+          <GridToolbarExport />
+        </FlexBetween>
+        <TextField
+          label="Search..."
+          sx={{ mb: "0.5rem", width: "15rem" }}
+          onChange={(e) => setSearchInput(e.target.value)}
+          value={searchInput}
+          variant="standard"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => {
+                    setSearch(searchInput);
+                    setSearchInput("");
+                  }}
+                >
+                  <Search />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </FlexBetween>
+    </GridToolbarContainer>
   );
 };
 
-export default CustomColumnMenu;
+export default DataGridCustomToolbar;
