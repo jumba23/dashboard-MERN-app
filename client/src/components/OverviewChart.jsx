883db70046d49a2d735df43a3145ba74f1d530c6
Goran SD <g.cvetic23@gmail.com>
2023-02-07 15:09:34 -0800

    overview page started

--- client/src/components/OverviewChart.jsx
+++ client/src/components/OverviewChart.jsx
@@ -2,10 +2,10 @@ import React, { useMemo } from "react";
 import { ResponsiveLine } from "@nivo/line";
 import { useTheme } from "@mui/material";
 import { useGetSalesQuery } from "../state/api";
-
 const OverviewChart = ({ isDashboard = false, view }) => {
   const theme = useTheme();
   const { data, isLoading } = useGetSalesQuery();
+  console.log("daily data", data);
 
   const [totalSalesLine, totalUnitsLine] = useMemo(() => {
     if (!data) return [];
