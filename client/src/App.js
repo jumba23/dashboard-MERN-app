d98d260a884df4f95d668b135dddd1691a0afa64
Goran SD <g.cvetic23@gmail.com>
2023-02-07 18:29:20 -0800

    Breakdown pie chart added

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import Transactions from "./scenes/transactions/index.jsx";
 import Geography from "./scenes/geography/index.jsx";
 import Overview from "./scenes/overview/index.jsx";
 import Daily from "./scenes/daily/index.jsx";
+import Monthly from "./scenes/monthly/index.jsx";
+import Breakdown from "./scenes/breakdown/index.jsx";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -40,6 +42,8 @@ function App() {
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
               <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
+              <Route path="/breakdown" element={<Breakdown />} />
             </Route>
           </Routes>
         </ThemeProvider>
